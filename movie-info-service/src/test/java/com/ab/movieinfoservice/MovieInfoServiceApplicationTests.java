a66dd0dcbaaa83839a88b185aa622a9dde25c7a6
Arvind Bhaskar <arvindbhaskar116@gmail.com>
2022-05-10 14:17:17 +0530

    Step1 - Create 3 spring boot application
    App1 gets data from user and request movie rating for that user from App2 and movie details from App3

---
+++ movie-info-service/src/test/java/com/ab/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
